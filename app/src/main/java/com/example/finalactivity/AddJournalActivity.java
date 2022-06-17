793334066517bf4ccac50227d81f5747556c88ef
Ederlie Rose Leoncio <ederlierose11@gmail.com>
2022-06-17 20:31:13 +0800

    june 17, 2022 pt2

---
+++ app/src/main/java/com/example/finalactivity/AddJournalActivity.java
@@ -0,0 +1,47 @@
+package com.example.finalactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import io.realm.Realm;
+
+public class AddJournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_journal);
+
+        EditText titleInput = findViewById(R.id.titleinput);
+        EditText descriptionInput = findViewById(R.id.descriptioninput);
+        MaterialButton savebtn = findViewById(R.id.savejournal);
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        savebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String title = titleInput.getText().toString();
+                String description =descriptionInput.getText().toString();
+                long createdTime = System.currentTimeMillis();
+
+                realm.beginTransaction();
+                Journal journal = realm.createObject(Journal.class);
+                journal.setTitle(title);
+                journal.setDescription(description);
+                journal.setCreatedTime(createdTime);
+                realm.commitTransaction();
+                Toast.makeText(getApplicationContext(), "Journal saved", Toast.LENGTH_SHORT).show();
+                finish();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/finalactivity/QuotesActivity.java
@@ -0,0 +1,33 @@
+package com.example.finalactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class QuotesActivity extends AppCompatActivity {
+
+    private Button startbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotes);
+
+        startbtn = (Button) findViewById(R.id.quotesnext);
+        startbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
