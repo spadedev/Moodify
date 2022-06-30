661ec4b4107bf73b2878e512f0c0278f96ce2c13
Ederlie Rose Leoncio <ederlierose11@gmail.com>
2022-06-30 19:24:13 +0800

    june 30, 2022

---
+++ app/src/main/java/com/example/finalactivity/Mood.java
@@ -0,0 +1,20 @@
+package com.example.finalactivity;
+
+public class Mood {
+    private String moodMessage;
+    private Integer moodColor;
+
+    public String getMoodMessage() {
+        return moodMessage;
+    }
+
+    public Integer getMoodColor() {
+        return moodColor;
+    }
+
+
+    Mood(String moodMessage, Integer moodColor) {
+        this.moodMessage = moodMessage;
+        this.moodColor = moodColor;
+    }
+}
