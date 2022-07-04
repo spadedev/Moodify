f3647bf9c54cb3fe0a50de6e088b8ce82b9f67db
Ederlie Rose Leoncio <ederlierose11@gmail.com>
2022-07-04 22:18:40 +0800

    july 4

---
+++ app/src/main/java/com/example/finalactivity/Model.java
@@ -0,0 +1,25 @@
+package com.example.finalactivity;
+
+public class Model {
+    String description;
+    String time;
+
+    public Model(){
+
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDesc() {
+        return description;
+    }
+    public String getTime() {
+        return time;
+    }
+}
